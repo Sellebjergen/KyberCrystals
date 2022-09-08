ae507c22f680d1a525a7206dfd2a6c183064898c
sellebjergen <sellebjergen@gmail.com>
2022-09-09 00:23:56 +0200

    Renamed the Eta constant

--- KyberCrystals/Params.cs
+++ KyberCrystals/Params.cs
@@ -6,7 +6,7 @@ public class Params
     public int K { get; set; }
     public int Q { get; set; }
     public int Eta1 { get; set; }
-    public int Eta2A { get; set; }
+    public int Eta2 { get; set; }
     public int Du { get; set; }
     public int Dv { get; set; }
 }
