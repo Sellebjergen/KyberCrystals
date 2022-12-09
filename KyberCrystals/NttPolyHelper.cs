a4cc8e2fc11e6b0cf6d9843f7fe28c65b4087e57
sellebjergen <sellebjergen@gmail.com>
2022-12-09 14:24:51 +0100

    Removed indentation

--- KyberCrystals/NttPolyHelper.cs
+++ KyberCrystals/NttPolyHelper.cs
@@ -9,20 +9,20 @@ public class NttPolyHelper
     private const short MontR2 = 1353;
 
     public readonly List<BigInteger> NttZetas = new()
-        // todo: calc all of these dynamically.
-        {
-            // Uses mont with R = 2^16 which have inverse 169 in mod 3329
-            2285, 2571, 2970, 1812, 1493, 1422, 287, 202, 3158, 622, 1577, 182, 962,
-            2127, 1855, 1468, 573, 2004, 264, 383, 2500, 1458, 1727, 3199, 2648, 1017,
-            732, 608, 1787, 411, 3124, 1758, 1223, 652, 2777, 1015, 2036, 1491, 3047,
-            1785, 516, 3321, 3009, 2663, 1711, 2167, 126, 1469, 2476, 3239, 3058, 830,
-            107, 1908, 3082, 2378, 2931, 961, 1821, 2604, 448, 2264, 677, 2054, 2226,
-            430, 555, 843, 2078, 871, 1550, 105, 422, 587, 177, 3094, 3038, 2869, 1574,
-            1653, 3083, 778, 1159, 3182, 2552, 1483, 2727, 1119, 1739, 644, 2457, 349,
-            418, 329, 3173, 3254, 817, 1097, 603, 610, 1322, 2044, 1864, 384, 2114, 3193,
-            1218, 1994, 2455, 220, 2142, 1670, 2144, 1799, 2051, 794, 1819, 2475, 2459,
-            478, 3221, 3021, 996, 991, 958, 1869, 1522, 1628
-        };
+    // todo: calc all of these dynamically.
+    {
+        // Uses mont with R = 2^16 which have inverse 169 in mod 3329
+        2285, 2571, 2970, 1812, 1493, 1422, 287, 202, 3158, 622, 1577, 182, 962,
+        2127, 1855, 1468, 573, 2004, 264, 383, 2500, 1458, 1727, 3199, 2648, 1017,
+        732, 608, 1787, 411, 3124, 1758, 1223, 652, 2777, 1015, 2036, 1491, 3047,
+        1785, 516, 3321, 3009, 2663, 1711, 2167, 126, 1469, 2476, 3239, 3058, 830,
+        107, 1908, 3082, 2378, 2931, 961, 1821, 2604, 448, 2264, 677, 2054, 2226,
+        430, 555, 843, 2078, 871, 1550, 105, 422, 587, 177, 3094, 3038, 2869, 1574,
+        1653, 3083, 778, 1159, 3182, 2552, 1483, 2727, 1119, 1739, 644, 2457, 349,
+        418, 329, 3173, 3254, 817, 1097, 603, 610, 1322, 2044, 1864, 384, 2114, 3193,
+        1218, 1994, 2455, 220, 2142, 1670, 2144, 1799, 2051, 794, 1819, 2475, 2459,
+        478, 3221, 3021, 996, 991, 958, 1869, 1522, 1628
+    };
 
     public readonly List<BigInteger> NttZetasInv = new()
     {
@@ -101,7 +101,7 @@ public class NttPolyHelper
         var u = a * 62209; // todo 62209 -> param.qInv
         var t = u * 3329; // todo 3329 -> param.q
         t = a - t;
-        t >>= 16; // TODO: is this due to montgomery factor of 2^16?
+        t >>= 16; // TODO: is this due to montgomery factor of 2^mont
         t %= 3329; // TODO: kyber param
 
         if (t < 0)
@@ -113,14 +113,14 @@ public class NttPolyHelper
     private BigInteger BarrettReduce(BigInteger a)
     {
         BigInteger t;
-        const long shift = (long)1 << 26;
+        const long shift = (long) 1 << 26;
         var v = (BigInteger)((shift + 3329 / 2) / 3329); // todo 3329 -> param.q
         t = (v * a) >> 26;
         t *= 3329; // todo 3329 -> param.q
         return a - t;
     }
 
-    public BigInteger FromMontgomery(BigInteger a)
+    private BigInteger FromMontgomery(BigInteger a)
     {
         return BigInteger.ModPow(a * 169, 1, 3329); // params 3329 and inverse mont 169
     }
